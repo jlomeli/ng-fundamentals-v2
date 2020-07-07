a74597d9353a9dffb2999ae60cbede63ec1481dd
Jesus Lomeli <jesus.lomeli@arrow.com>
2020-07-07 17:09:52 -0500

    Getting started with Angular

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-events',
-  templateUrl: './app.component.html',
+  templateUrl: './event-app.component.html',
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals-v2';
