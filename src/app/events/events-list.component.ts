6abcea23d55f6b4ba475776e92456236250f82aa
Jesus Lomeli <jesus.lomeli@arrow.com>
2020-07-09 16:22:10 -0500

    Added 3rd party library as a service

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
   selector: 'app-events-list',
@@ -8,9 +9,16 @@ import { EventService } from './shared/event.service';
 })
 export class EventsListComponent implements OnInit {
   events: any = [];
-  constructor(private eventService: EventService) {}
+  constructor(
+    private eventService: EventService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName: string) {
+    this.toastr.success(eventName);
   }
 }
